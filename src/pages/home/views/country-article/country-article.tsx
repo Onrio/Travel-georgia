fc648adc695a2a0203cd49fff7de4a40b31bd489
Anrio <a.silagadze96@gmail.com>
2024-10-21 01:16:08 +0400

    Assignment Tenth fix

--- src/pages/home/views/country-article/country-article.tsx
+++ src/pages/home/views/country-article/country-article.tsx
@@ -3,14 +3,13 @@ import { countries } from "@/dammyData";
 import style from "./style.module.css";
 import mainStyle from "@/style/index.module.css";
 
-
 const CountryArticle = () => {
   const { id } = useParams();
 
-  const articleData = countries.find((country) => country.id == id);
+  const articleData = countries.find((country) => country.id === id);
 
   if (!articleData) {
-    return <div>article not found :/</div>;
+    return <div>Article not found :/</div>;
   }
 
   return (
@@ -18,13 +17,13 @@ const CountryArticle = () => {
       <div className={mainStyle["container"]}>
         <div className={style["country-view-row"]}>
           <div className={style["country-view-info"]}>
-            <h2>{articleData.name}</h2>
-            <p>{articleData.about}</p>
+            <h2>{articleData.name.ka}</h2>
+            <p>{articleData.about.ka}</p>
           </div>
           <div className={style["country-view-image"]}>
             <img
               src={`/src/assets/vineyards/${articleData.image}`}
-              alt={`vineyard in ${articleData.name}`}
+              alt={`Vineyard in ${articleData.name.en}`}
             />
           </div>
         </div>
@@ -32,4 +31,5 @@ const CountryArticle = () => {
     </section>
   );
 };
+
 export default CountryArticle;
